505c1b4ad864470494456f9182cfc186981f6c8c
ProGM <progiemmeh@gmail.com>
2014-11-02 19:25:06 +0900

    Fixing compilation with pip and travis setup.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
+__module_name__ = 'py-seam-merging'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 import runpy
-from Cython.Build import cythonize
-import numpy
+from distutils.extension import Extension
 import sys
 
 if sys.version_info < (2, 6):
@@ -15,15 +19,44 @@ __version_str__ = runpy.run_path("seammerging/version.py")["version"]
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-include_path = [numpy.get_include()]
 
-extensions = [
-    Extension('native_seam_merging', ['seammerging/src/*.pyx'],
-              include_dirs=include_path
-              )
-]
+class CythonExtList(list):
+    def __init__(self):
+        self._list = None
+    def c_list(self):
+        if self._list is None:
+            import numpy
+            from Cython.Build import cythonize
+            include_path = [numpy.get_include()]
+            self._list = cythonize([
+              Extension('native_seam_merging', ['seammerging/src/*.pyx'],
+                  include_dirs=include_path
+              )])
+        return self._list
+    def __iter__(self):
+        _list = self.c_list()
+        for e in _list:
+            yield e
+    def __getitem__(self, ii):
+        return self.c_list()[ii]
+    def __delitem__(self, ii):
+        del self.c_list()[ii]
+    def __setitem__(self, ii, val):
+        return self.c_list()[ii]
+    def __str__(self):
+        return self.__repr__()
+    def __repr__(self):
+        return """<CythonExtList %s>""" % self.c_list()
+    def __len__(self):
+        return len(self.c_list())
+    def append(self, val):
+        self.c_list().append(val)
 
-setup(name='py-seam-merging',
+
+extensions = CythonExtList()
+
+
+setup(name=__module_name__,
       version=__version_str__,
       description='Seam merging for images implementation.',
       author='Piero Dotti, Paolo Guglielmini',
@@ -31,7 +64,7 @@ setup(name='py-seam-merging',
       license='MIT',
       url='http://github.com/PnProductions/py-seam-merging',
       packages=['seammerging'],
-      ext_modules=cythonize(extensions),
+      ext_modules=extensions,
       install_requires=requirements,
       setup_requires=requirements
       )
